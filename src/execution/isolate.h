3bdf8c6d6b98ef424e3975611733f7147e479de4
Dmitry Bezhetskov <dima00782@gmail.com>
2025-11-13 17:29:26 +0100

    Introduce an isolate materialization API.
    After cloning an isolate group, an individual isolate must be
    materialized before use.  Materialization initializes all internal
    fields of the cloned isolate without modifying its underlying memory,
    making the isolate usable.

--- src/execution/isolate.h
+++ src/execution/isolate.h
@@ -2430,6 +2430,8 @@ class V8_EXPORT_PRIVATE Isolate final : private HiddenFactory {
 
   void PrintNumberStringCacheStats(const char* comment, bool final_summary);
 
+  bool InitClone(Isolate* target);
+
  private:
   explicit Isolate(IsolateGroup* isolate_group);
   ~Isolate();
@@ -2801,6 +2803,7 @@ class V8_EXPORT_PRIVATE Isolate final : private HiddenFactory {
   void ClearEmbeddedBlob();
 
   void InitializeBuiltinJSDispatchTable();
+  void CloneBuiltinTable(Isolate* original);
 
   const uint8_t* embedded_blob_code_ = nullptr;
   uint32_t embedded_blob_code_size_ = 0;
